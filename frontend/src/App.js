5bb4fb3a8d24720a810ff6e718d77a2bc8ad857c
Thiago Guerrero <tgbalera@hotmail.com>
2021-01-13 12:48:56 -0300

    add google places fetch from backend and add marker visualization
    Signed-off-by: Thiago Guerrero <tgbalera@hotmail.com>

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,13 @@
 import { Fragment, useState, useEffect } from 'react';
-import { GoogleMap, LoadScript } from '@react-google-maps/api';
+import { GoogleMap, LoadScript, Marker } from '@react-google-maps/api';
+import EstablishmentsService from './services/google_list_of_establishments';
 
 function App() {
   const { REACT_APP_GOOGLE_API_KEY } = process.env;
+
   const [latitude, setLatitude] = useState(0);
   const [longitude, setLongitude] = useState(0);
+  const [locations, setLocations] = useState([]);
 
   async function setCurrentLocation() {
     await navigator.geolocation.getCurrentPosition((position) => {
@@ -15,8 +18,14 @@ function App() {
     })
   }
 
+  async function loadCoffeeShops() {
+    const response = await EstablishmentsService.index(latitude, longitude);
+    setLocations(response.data.results);
+  }
+
   useEffect(() => {
     setCurrentLocation();
+    loadCoffeeShops();
   }, []);
 
   return (
@@ -26,6 +35,17 @@ function App() {
         mapContainerStyle={{height: "100vh", width: "100%"}}
         zoom={15}
         center={{lat: latitude, lng: longitude}}>
+          {
+            locations.map((location, index) => {
+              return (<Marker 
+                key={index} 
+                icon="/images/coffee-pin.svg"
+                title={location.name} 
+                animation="4"
+                position={{lat: location.geometry.location.lat, lng: location.geometry.location.lng}}/>)
+            })
+          }
+          <Marker icon="/images/my-location-pin.svg" title="Seu local" animation="2" position={{lat: latitude, lng: longitude}} />
         </GoogleMap>
       </LoadScript>
     </Fragment>
